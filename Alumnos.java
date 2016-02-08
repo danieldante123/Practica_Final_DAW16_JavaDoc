b04be87411dd7918a4853bce7433c29592b4e5e1
danieldante123 <daniel.informatica.webmaster@gmail.com>
2016-02-08 23:42:44 +0100

    Create Alumnos.java

---
+++ Alumnos.java
@@ -0,0 +1,148 @@
+
+package practicaDAW;
+
+import java.awt.Color;
+import java.util.List;
+
+/**
+ * Clase que deben implementar los alumnos para completar la practica de GIT
+ * @author DaniDespacho
+ * @version 1.0
+ * 
+ */
+
+public class Alumnos {
+    
+    public Color metodoProfesor(List<String> atributos)
+    {
+        return null;
+        
+    }
+    
+    public Color metodoCarlos(List<String> atributos)
+    {
+        
+        return null;
+        
+    }
+    
+    public Color metodoAntonioB(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoFrancisco(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoJuanjo(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoDavid(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoIsidoro(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoVeronica(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoPabloE(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoRuben(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoJuanCarlos(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoAntonioM(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoJaime(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoVictor(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoAlejandro(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoPabloR(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+    
+    public Color metodoDaniel(List<String> atributos)
+    {
+        
+        
+        return null;
+        
+    }
+}
