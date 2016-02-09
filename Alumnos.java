0aa6932918a3e4e72e25b352b79a83f1f077e62f
danieldante123 <daniel.informatica.webmaster@gmail.com>
2016-02-09 19:28:28 +0100

    Update Alumnos.java

--- Alumnos.java
+++ Alumnos.java
@@ -16,6 +16,8 @@ public class Alumnos {
     public Color metodoProfesor(List<String> atributos)
     {
         atributos.add("Nombre: Daniel");
+        atributos.add("Apellidos: López Lozano");
+        
         return null;
         
     }
